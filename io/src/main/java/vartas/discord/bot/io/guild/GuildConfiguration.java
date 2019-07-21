1b3e36876c275769c0724f4f136dba12aac69974
Zavarov <25961215+Zavarov@users.noreply.github.com>
2019-07-21 03:01:22 +0200

    Removed the old copyright.

--- io/src/main/java/vartas/discord/bot/io/guild/GuildConfiguration.java
+++ io/src/main/java/vartas/discord/bot/io/guild/GuildConfiguration.java
@@ -15,22 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package vartas.discord.bot.io.guild;/*
- * Copyright (C) 2017 u/Zavarov
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+package vartas.discord.bot.io.guild;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
