9a7662befd0e5ca038aca2f3edaa6d88c907ae01
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:10:06 +0200

    Specify getAbout().

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/GuildView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/GuildView.java
@@ -1,8 +1,11 @@
 package zav.discord.blanc.view;
 
+import zav.discord.blanc.databind.Guild;
+
 import java.util.Collection;
 
 public interface GuildView {
+  Guild getAbout();
   MemberView getMember(long id);
   MemberView getSelfMember();
   TextChannelView getTextChannel(long id);
