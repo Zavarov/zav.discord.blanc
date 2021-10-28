b79f170268baeeb3e48ca1f5151a2a3e0058ff2d
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:05:30 +0200

    Specify getPermissions() & getRoles().

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MemberView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MemberView.java
@@ -1,9 +1,14 @@
 package zav.discord.blanc.view;
 
+import zav.discord.blanc.Permission;
+
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 public interface MemberView extends UserView {
+  Set<Permission> getPermissions();
+  Set<RoleView> getRoles();
   void modifyRoles(Collection<RoleView> rolesToAdd, Collection<RoleView> rolesToRemove);
   default void addRoles(Collection<RoleView> roles) {
     modifyRoles(roles, Collections.emptySet());
