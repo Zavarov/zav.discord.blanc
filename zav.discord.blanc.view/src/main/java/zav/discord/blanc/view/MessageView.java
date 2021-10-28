646bdcb796b9ce73f81b3d5d8ae7f5a7f490edd0
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:05:45 +0200

    Specify getShard() & getMessageChannel().

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MessageView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MessageView.java
@@ -3,5 +3,7 @@ package zav.discord.blanc.view;
 import zav.discord.blanc.databind.Message;
 
 public interface MessageView {
+  MessageChannelView getMessageChannel();
+  ShardView getShard();
   Message getAbout();
 }
