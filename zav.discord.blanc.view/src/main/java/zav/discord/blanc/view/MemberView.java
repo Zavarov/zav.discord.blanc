a52d2702b8d2c483847b8c159c25f15fba9d83b9
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:04:06 +0200

    Specify methods for role modification.

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MemberView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MemberView.java
@@ -1,4 +1,20 @@
 package zav.discord.blanc.view;
 
-public interface MemberView extends UserView{
+import java.util.Collection;
+import java.util.Collections;
+
+public interface MemberView extends UserView {
+  void modifyRoles(Collection<RoleView> rolesToAdd, Collection<RoleView> rolesToRemove);
+  default void addRoles(Collection<RoleView> roles) {
+    modifyRoles(roles, Collections.emptySet());
+  }
+  default void addRole(RoleView role) {
+    addRoles(Collections.singleton(role));
+  }
+  default void removeRoles(Collection<RoleView> roles) {
+    modifyRoles(Collections.emptySet(), roles);
+  }
+  default void removeRole(RoleView role) {
+    removeRoles(Collections.singleton(role));
+  }
 }
