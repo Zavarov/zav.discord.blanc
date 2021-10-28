2e8bfbc9500def53019a626ea823e05cfdbeb064
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:08:59 +0200

    Specify shutdown(), getAbout() & getSelfUser().

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/ShardView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/ShardView.java
@@ -1,8 +1,13 @@
 package zav.discord.blanc.view;
 
+import zav.discord.blanc.Shard;
+
 import java.util.Collection;
 
 public interface ShardView {
+  void shutdown();
+  Shard getAbout();
+  SelfUserView getSelfUser();
   Collection<GuildView> getGuilds();
   GuildView getGuild(long id);
   UserView getUser(long id);
