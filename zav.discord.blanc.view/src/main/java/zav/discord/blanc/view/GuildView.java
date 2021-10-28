0be29c01b1c42f336db8a7ed6b08da54bbcc82ae
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:10:33 +0200

    Specify updateActivity(), updateBlacklist(), leave() & canInteract().

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/GuildView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/GuildView.java
@@ -1,11 +1,21 @@
 package zav.discord.blanc.view;
 
 import zav.discord.blanc.databind.Guild;
+import zav.discord.blanc.databind.Role;
+import zav.discord.blanc.databind.TextChannel;
 
+import java.awt.image.BufferedImage;
 import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public interface GuildView {
   Guild getAbout();
+  void updateActivity();
+  void updateBlacklist(Pattern pattern);
+  void leave();
+  boolean canInteract(MemberView member, Role role);
+  BufferedImage getActivity(List<TextChannel> channels);
   MemberView getMember(long id);
   MemberView getSelfMember();
   TextChannelView getTextChannel(long id);
