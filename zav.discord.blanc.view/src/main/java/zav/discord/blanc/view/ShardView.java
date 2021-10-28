f4c3a23bb59b53d0d659956cf2c592f479f77399
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:08:32 +0200

    Rename getAllGuilds() to getGuilds().

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/ShardView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/ShardView.java
@@ -3,7 +3,7 @@ package zav.discord.blanc.view;
 import java.util.Collection;
 
 public interface ShardView {
-  Collection<GuildView> getAllGuilds();
+  Collection<GuildView> getGuilds();
   GuildView getGuild(long id);
   UserView getUser(long id);
 }
