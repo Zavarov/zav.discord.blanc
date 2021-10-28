f1e43e73a6f25b631f74d864233f048f2d552fb0
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:09:36 +0200

    Specify getSelfMember() & getRoles().

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/GuildView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/GuildView.java
@@ -1,7 +1,11 @@
 package zav.discord.blanc.view;
 
+import java.util.Collection;
+
 public interface GuildView {
   MemberView getMember(long id);
+  MemberView getSelfMember();
   TextChannelView getTextChannel(long id);
+  Collection<RoleView> getRoles();
   RoleView getRole(long id);
 }
