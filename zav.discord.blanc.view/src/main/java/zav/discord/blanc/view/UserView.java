f42d675f330ef55b19c5012e368c162791485419
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-28 22:07:01 +0200

    Specify getAbout() & getRanks().

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/UserView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/UserView.java
@@ -1,5 +1,15 @@
 package zav.discord.blanc.view;
 
+import zav.discord.blanc.Rank;
+import zav.discord.blanc.databind.User;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public interface UserView {
+  User getAbout();
   PrivateChannelView getPrivateChannel();
+  default Set<Rank> getRanks() {
+    return getAbout().getRank().stream().map(Rank::valueOf).collect(Collectors.toUnmodifiableSet());
+  }
 }
