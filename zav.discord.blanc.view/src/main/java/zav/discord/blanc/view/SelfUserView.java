f8ae85575e62f485be1c431fb4f6853c04807101
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-29 20:33:12 +0200

    Change the argument type of setAvatar(...) from String to BufferedImage.

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/SelfUserView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/SelfUserView.java
@@ -2,7 +2,9 @@ package zav.discord.blanc.view;
 
 import zav.discord.blanc.databind.User;
 
+import java.awt.image.BufferedImage;
+
 public interface SelfUserView extends UserView {
   User getAbout();
-  void setAvatar(String data);
+  void setAvatar(BufferedImage image);
 }
