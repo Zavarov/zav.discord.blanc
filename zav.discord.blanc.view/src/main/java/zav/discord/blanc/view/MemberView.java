b754d3c745b4c48e453f3f5e5f9cdd5426d0f1ac
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-29 19:40:44 +0200

    Replace RoleView with Role in add/remove/modifyRoles signature.

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MemberView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MemberView.java
@@ -1,6 +1,7 @@
 package zav.discord.blanc.view;
 
 import zav.discord.blanc.Permission;
+import zav.discord.blanc.databind.Role;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -9,17 +10,17 @@ import java.util.Set;
 public interface MemberView extends UserView {
   Set<Permission> getPermissions();
   Set<RoleView> getRoles();
-  void modifyRoles(Collection<RoleView> rolesToAdd, Collection<RoleView> rolesToRemove);
-  default void addRoles(Collection<RoleView> roles) {
+  void modifyRoles(Collection<Role> rolesToAdd, Collection<Role> rolesToRemove);
+  default void addRoles(Collection<Role> roles) {
     modifyRoles(roles, Collections.emptySet());
   }
-  default void addRole(RoleView role) {
+  default void addRole(Role role) {
     addRoles(Collections.singleton(role));
   }
-  default void removeRoles(Collection<RoleView> roles) {
+  default void removeRoles(Collection<Role> roles) {
     modifyRoles(Collections.emptySet(), roles);
   }
-  default void removeRole(RoleView role) {
+  default void removeRole(Role role) {
     removeRoles(Collections.singleton(role));
   }
 }
