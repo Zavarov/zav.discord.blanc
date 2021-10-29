4f5d75366135c28c01950263d58064e87996e4d5
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-29 23:07:02 +0200

    Specify methods for creating webhooks.

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/TextChannelView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/TextChannelView.java
@@ -2,4 +2,8 @@ package zav.discord.blanc.view;
 
 public interface TextChannelView extends MessageChannelView {
   void updateSubreddit(String subreddit);
+  WebhookView getWebhook(boolean create);
+  default WebhookView getWebhook() {
+    return getWebhook(false);
+  }
 }
