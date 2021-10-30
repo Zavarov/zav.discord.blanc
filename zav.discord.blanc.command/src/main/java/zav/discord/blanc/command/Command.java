557803438b00f551ed390747d308a6eb85a759fc
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-10-30 13:30:19 +0200

    Add postConstruct() method for resolving arguments, rather than the constructor.

--- zav.discord.blanc.command/src/main/java/zav/discord/blanc/command/Command.java
+++ zav.discord.blanc.command/src/main/java/zav/discord/blanc/command/Command.java
@@ -1,7 +1,13 @@
 package zav.discord.blanc.command;
 
+import zav.discord.blanc.command.parser.Argument;
 import zav.discord.blanc.job.Job;
+
+import java.util.List;
 
 public interface Command extends Job {
   void validate() throws InvalidCommandException;
+  default void postConstruct(List<? extends Argument> args) {
+  
+  }
 }
