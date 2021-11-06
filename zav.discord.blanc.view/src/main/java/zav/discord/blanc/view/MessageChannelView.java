fb65153148423e4ab71d960d67aaacfcd348a9d7
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-11-06 19:49:56 +0100

    Add send(Link) method.

--- zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MessageChannelView.java
+++ zav.discord.blanc.view/src/main/java/zav/discord/blanc/view/MessageChannelView.java
@@ -2,9 +2,9 @@ package zav.discord.blanc.view;
 
 import zav.discord.blanc.Argument;
 import zav.discord.blanc.databind.User;
+import zav.jrc.databind.Link;
 
 import java.awt.image.BufferedImage;
-import java.math.BigDecimal;
 
 public interface MessageChannelView {
   // Views
@@ -16,4 +16,5 @@ public interface MessageChannelView {
   void send(BufferedImage image, String imageName);
   void send(Object content);
   void send(User user);
+  void send(Link link);
 }
