cea1d385a5f901169984ea9e714cf55d0cdb674c
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-11-20 17:24:24 +0100

    Load the credentials when creating a Shard instance.

--- zav.discord.blanc.api/src/main/java/zav/discord/blanc/Shard.java
+++ zav.discord.blanc.api/src/main/java/zav/discord/blanc/Shard.java
@@ -1,4 +1,20 @@
 package zav.discord.blanc;
 
-public interface Shard {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import zav.discord.blanc.databind.io.Credentials;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Shard {
+  private final Credentials credentials;
+  
+  public Shard() throws IOException {
+    ObjectMapper om = new ObjectMapper();
+    this.credentials = om.readValue(new File("credentials.json"), Credentials.class);
+  }
+  
+  public Credentials getCredentials() {
+    return credentials;
+  }
 }
