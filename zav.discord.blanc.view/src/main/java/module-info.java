adc46e7ff8aa9fc3b245ad39210176893ac6b229
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-11-23 18:25:46 +0100

    Add databind dependency.

--- zav.discord.blanc.view/src/main/java/module-info.java
+++ zav.discord.blanc.view/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module zav.discord.blanc.view {
   requires zav.discord.blanc.databind;
   requires java.desktop;
   requires zav.discord.blanc.api;
+  requires zav.jrc.databind;
   
   exports zav.discord.blanc.view;
 }
