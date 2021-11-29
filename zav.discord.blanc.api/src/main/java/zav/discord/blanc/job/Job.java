05546efeb2e88e9c6b3d0320ed6881eb5cb27dde
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-11-29 20:12:28 +0100

    Update JavaDoc

--- zav.discord.blanc.api/src/main/java/zav/discord/blanc/job/Job.java
+++ zav.discord.blanc.api/src/main/java/zav/discord/blanc/job/Job.java
@@ -1,12 +1,14 @@
 package zav.discord.blanc.job;
 
+import java.util.concurrent.Callable;
+
 /**
  * This interface should be implement by any class that should be executed by an asynchronous thread
  * within the application.<br>
- * It is heavily inspired by the {@link Runnable} interface, with the only exception being that the
- * {@code run} method is allowed to throw an exception.
+ * It is heavily inspired by both the {@link Callable} and {@link Runnable} interface.
  *
  * @see Runnable
+ * @see Callable
  */
 public interface Job {
   void run() throws Exception;
