b378dfd6ad6ab16d812a57b543317e72a3110186
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-12-19 12:55:09 +0100

    Modularize zav.discord.blanc.db.

---
+++ zav.discord.blanc.db/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module zav.discord.blanc.db {
+  requires static org.eclipse.jdt.annotation;
+  
+  requires java.sql;
+  requires com.fasterxml.jackson.databind;
+  requires org.apache.commons.lang3;
+  requires org.apache.logging.log4j;
+  requires zav.discord.blanc.databind;
+  
+  exports zav.discord.blanc.db;
+  
+  opens guild;
+  opens role;
+  opens textchannel;
+  opens user;
+  opens webhook;
+}
