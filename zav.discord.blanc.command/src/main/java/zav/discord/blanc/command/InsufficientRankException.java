eff23004d78285d6bd2ad35ba97db9b229cd9ae8
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-12-20 22:24:26 +0100

    Add a proper error message to the InsufficientRankException.

--- zav.discord.blanc.command/src/main/java/zav/discord/blanc/command/InsufficientRankException.java
+++ zav.discord.blanc.command/src/main/java/zav/discord/blanc/command/InsufficientRankException.java
@@ -1,5 +1,7 @@
 package zav.discord.blanc.command;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * This exception is thrown whenever a user executes a command for which they lack the required
  * rank.
@@ -7,4 +9,12 @@ package zav.discord.blanc.command;
  * @see Rank
  */
 public class InsufficientRankException extends InvalidCommandException {
+  public InsufficientRankException(Rank... ranks) {
+    super(getMessage(ranks));
+  }
+  
+  private static String getMessage(Rank... ranks) {
+    return "You require the following ranks in order to execute this command: "
+          + StringUtils.join(ranks, ",");
+  }
 }
--- zav.discord.blanc.command/src/main/java/zav/discord/blanc/command/InvalidCommandException.java
+++ zav.discord.blanc.command/src/main/java/zav/discord/blanc/command/InvalidCommandException.java
@@ -4,4 +4,11 @@ package zav.discord.blanc.command;
  * Base class for all command-related exception.<br>
  */
 public abstract class InvalidCommandException extends Exception {
+  public InvalidCommandException() {
+    super();
+  }
+  
+  public InvalidCommandException(String message) {
+    super(message);
+  }
 }
