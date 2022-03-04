5395c4725e904ce3529d650b4d6517a8754efb75
Zavarov <25961215+Zavarov@users.noreply.github.com>
2022-03-04 16:24:21 +0100

    Remove unused imports.

--- zav.discord.blanc.api/src/test/java/zav/discord/blanc/api/internal/SiteComponentListenerTest.java
+++ zav.discord.blanc.api/src/test/java/zav/discord/blanc/api/internal/SiteComponentListenerTest.java
@@ -17,7 +17,6 @@
 package zav.discord.blanc.api.internal;
 
 import static org.apache.commons.lang3.StringUtils.EMPTY;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
