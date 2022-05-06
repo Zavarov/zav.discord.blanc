b0a946f9805d272e277f2d61b216867c3e81041e
Zavarov <25961215+Zavarov@users.noreply.github.com>
2022-05-06 21:12:41 +0200

    Cleanup module dependencies and rename WebHook to Webhook.

--- zav.discord.blanc.databind/src/main/java/module-info.java
+++ zav.discord.blanc.databind/src/main/java/module-info.java
@@ -1,11 +1,10 @@
-@SuppressWarnings("Java9RedundantRequiresStatement") // IntelliJ seems to ignore generated classes...
 module zav.discord.blanc.databind {
   requires static org.eclipse.jdt.annotation;
   requires static org.jetbrains.annotations;
   
-  requires transitive com.fasterxml.jackson.annotation;
-  requires transitive com.fasterxml.jackson.databind;
-  requires transitive java.compiler;
+  requires com.fasterxml.jackson.annotation;
+  
+  requires java.compiler;
   
   exports zav.discord.blanc.databind.io;
   exports zav.discord.blanc.databind;
