662d09dd3e4111d6b87ecc028c72e24a0aa61990
Zavarov <25961215+Zavarov@users.noreply.github.com>
2022-05-26 19:12:36 +0200

    Enhance retain test case.
    In case the bot can't talk in a text channel, delete all webhooks from the database.

--- zav.discord.blanc.db/src/test/java/zav/discord/blanc/db/WebhookTableTest.java
+++ zav.discord.blanc.db/src/test/java/zav/discord/blanc/db/WebhookTableTest.java
@@ -175,9 +175,15 @@ public class WebhookTableTest extends AbstractTableTest {
     when(guild.getId()).thenReturn(Long.toString(entity.getGuildId()));
     when(textChannel.retrieveWebhooks()).thenReturn(action);
     when(textChannel.getId()).thenReturn(Long.toString(entity.getChannelId()));
-    when(textChannel.canTalk()).thenReturn(true);
     when(action.complete()).thenReturn(List.of(webhook));
-    
+  
+    when(textChannel.canTalk()).thenReturn(false);
+    db.put(entity);
+  
+    db.retain(guild);
+    assertFalse(db.contains(webhook));
+  
+    when(textChannel.canTalk()).thenReturn(true);
     db.put(entity);
     
     db.retain(guild);
